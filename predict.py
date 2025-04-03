88f3373872b97dafb89622f7fea4384b14eb205d
alice33-stu <146663710+alice33-stu@users.noreply.github.com>
2025-04-02 19:12:17 -0700

    Update predict.py

--- predict.py
+++ predict.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
-# 加载数据
+# load data
 data = pd.read_csv('bitcoin_price.csv', parse_dates=['timestamp']).sort_values(by='timestamp')
 data['Date'] = pd.to_datetime(data['timestamp'])
 data.set_index('Date', inplace=True)
-data = data[['close']]  # 使用正确的列名 'close'
-data = data.sort_index()  # 确保按日期升序排序
+data = data[['close']]  # use correct column name 'close'
+data = data.sort_index()  # ascending order 
 
-# 标准化数据
+# standardardize data 
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(data.values)
 
-# 创建数据集
+# create dataset 
 def create_dataset(data, time_step=1):
     X, Y = [], []
     for i in range(len(data) - time_step - 1):
@@ -27,7 +27,7 @@ def create_dataset(data, time_step=1):
 time_step = 100
 X, Y = create_dataset(scaled_data, time_step)
 
-# 拆分训练集和测试集
+# split train and test dataset 
 train_size = int(len(X) * 0.8)
 X_train, X_test = X[0:train_size], X[train_size:len(X)]
 Y_train, Y_test = Y[0:train_size], Y[train_size:len(Y)]
@@ -35,20 +35,20 @@ Y_train, Y_test = Y[0:train_size], Y[train_size:len(Y)]
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], 1)
 X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], 1)
 
-# 加载模型
+# load model 
 model = load_model('lstm_bitcoin.h5')
 
-# 进行预测
+# do prediction 
 train_predict = model.predict(X_train)
 test_predict = model.predict(X_test)
 
-# 反向变换数据到原始范围
+# inverse transform the data to the original range 
 train_predict = scaler.inverse_transform(train_predict)
 test_predict = scaler.inverse_transform(test_predict)
 Y_train_inv = scaler.inverse_transform([Y_train])
 Y_test_inv = scaler.inverse_transform([Y_test])
 
-# 绘制实际值与预测值对比图
+# plot the comparsion graph 
 train_index = data.index[:len(Y_train)]
 test_index = data.index[len(Y_train)+time_step:len(Y_train)+time_step+len(Y_test)]
 
